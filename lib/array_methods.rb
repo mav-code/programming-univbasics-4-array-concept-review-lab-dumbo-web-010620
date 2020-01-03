c607991ffc41c68265bb873ab310ea43c9763523
Mavi Toktamis-Paker <mavisansmembres@gmail.com>
2020-01-03 18:38:51 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,8 +3,9 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
+  candidateindex = 0
   array.length.times {
-    
+    if array.candidateindex > array.(candidateindex + 1)
   }
 end
 
